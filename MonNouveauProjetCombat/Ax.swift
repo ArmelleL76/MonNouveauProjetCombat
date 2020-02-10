335b4a69bdd7cfc88e3196f12cb232953803c62f
Armelle <armelle.lecerf@orange.fr>
2020-02-10 21:08:44 +0100

    ajout des filles de la classe weapon

---
+++ MonNouveauProjetCombat/Ax.swift
@@ -0,0 +1,14 @@
+//
+//  Ax.swift
+//  MonNouveauProjetCombat
+//
+//  Created by macmini-Armelle on 10/02/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class Ax : Weapon {
+    init() {
+        super.init(damage: 15)
+    }
+}
