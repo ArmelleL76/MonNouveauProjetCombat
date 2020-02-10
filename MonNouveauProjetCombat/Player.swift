e71161fddd1fa88bf83787806f0342fc6ce43efd
Armelle <armelle.lecerf@orange.fr>
2020-02-10 20:45:09 +0100

    Ajout de la classe Player

---
+++ MonNouveauProjetCombat/Player.swift
@@ -0,0 +1,24 @@
+//
+//  Player.swift
+//  MonNouveauProjetCombat
+//
+//  Created by macmini-Armelle on 10/02/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class Player {
+    var Name : String = ""
+    var warriors : [Warrior]
+    
+    init() {
+        warriors = []
+    }
+    func hasTeamAlive() -> Bool {
+        
+    }
+    
+    func selecFighter()-> Warrior{
+        
+    }
+}
