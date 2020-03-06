2c20edb62465a9980bde81dacc0f6f3850eb5f41
Armelle <armelle.lecerf@orange.fr>
2020-03-06 18:52:34 +0100

    modifs mineures dans la présentation du jeu

--- MonNouveauProjetCombat/Game.swift
+++ MonNouveauProjetCombat/Game.swift
@@ -19,7 +19,7 @@ class Game {
         print("1. Two players choose three fighters to make a warrior's team. We'll have then two teams.Each player choose a fighter in his own team to meet another fighter in the other team.\n\n")
         print("2. Fighters fight together or choose to improve their arms or to gain more lives. Sometimes a chest containing a weapon appears and a player randomly choosen can exchange his fighter's arm with the random one .At the end, the winner is the player with a fighter alive\n\n")
         
-        print("3. Here is the list of possible fighters : \n\n******************\n\nTeam1: \n\nViking1, Gladiator1 and Knight1\n\n******************\n\nTeam2:\n\nViking2, Gladiator2 and Knight2\n\n******************\n\n")
+        print("3. Here is the list of possible fighters : \n\n******************\n\nTeam1: \n\nthree fighters chosen among  viking, gladiator and knight\n\n******************\n\nTeam2:\n\nthree fighters chosen among  viking, gladiator and knight\n\n******************\n\n")
         
         print("Here is a suggestion of optionnal names for your warriors :   \nVikings'names : Sigrid, Thor, Bjorn, Odin, Ragnar, Magnum... \nGladiators'names : Lucius, Marcus, Titus, Sextus, Quintus, Servius...\nKnights'names : Gauvain, Arthur, Perceval, Lancelot, Tristan...\n\n")
         
