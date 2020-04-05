c4a962d511eaac7285a1b4f0675bc2e516b29093
Armelle <armelle.lecerf@orange.fr>
2020-04-05 21:57:24 +0200

    ajouts de commentaires dans la classe Warrior et réindentation

--- MonNouveauProjetCombat/Warrior.swift
+++ MonNouveauProjetCombat/Warrior.swift
@@ -19,10 +19,10 @@ class Warrior {
     init(life : Int, weapon : Weapon){
         self.life = life
         self.weapon = weapon
-        print("\nGive a name to the fighter: \n")
+        print("\nGive a name to the fighter: \n")  //give names to fighters
         self.name = readLine()!
         while Warrior.names.contains(self.name) {
-            print("That name already exist, please choose a new one:\n")
+            print("That name already exist, please choose a new one:\n") //ask to give another name if the given name already exist
             self.name = readLine()!
         }
         Warrior.names.append(self.name)
@@ -35,11 +35,11 @@ class Warrior {
             })
             .last ?? ""
     }
-    
+    //function which give damage to the opponent
     func actionOn (fighter : Warrior) {
         fighter.receiveDamage(damage: self.weapon.damage)
     }
-    
+    //function to calculate received damages
     func receiveDamage(damage : Int){
         self.life = self.life - damage
         //If life turns into negative value, life will be equal to zero
@@ -48,17 +48,17 @@ class Warrior {
             self.life = 0
         }
     }
-    
+    //function to increase power arm of the first fighter
     func increasePowerArm ()
     {
         self.weapon.damage += 25
         
     }
-    
+    //function to cure the fighter of player who play
     func cureOneself(){
         self.life +=  20 }
     
-    
+    //print state of fighter after an attack
     func printCharacteristic(){
         if life > 0 {
             print("\(name) is still alive, Life : \(life), force : \(weapon.damage)\n\n          ************\n")
