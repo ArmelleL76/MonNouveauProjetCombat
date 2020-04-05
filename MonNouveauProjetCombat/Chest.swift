88a3990f672ccf182bf4d6c55b12e5441ff11403
Armelle <armelle.lecerf@orange.fr>
2020-04-05 21:47:47 +0200

    ajout de commentaires dans la classe Chest et réindentation

--- MonNouveauProjetCombat/Chest.swift
+++ MonNouveauProjetCombat/Chest.swift
@@ -12,11 +12,12 @@ class Chest{
     init(randomStrenght : Int)
     {self.randomStrenght = randomStrenght}
     
+    //we call a random function to make the chest appear
     func appear()-> Bool {
         let random = Int.random(in: 1..<7)
         if random % 3 == 0 {
             print("\n\n*********** A CHEST HAS APPEARED !! ************\n\n")
-           
+            
             return true}
             
         else
@@ -24,12 +25,14 @@ class Chest{
             return false }
         
     }
+    //we calculate the randomStrenght of Chest's arm
     func armStrenght()->Int{
         let randomInteger = Int.random(in: 26..<45)
         self.randomStrenght = randomInteger
         print("The chest contains a weapon of power : \(randomStrenght)\n\nThe weapon of fighter1 will be exchanged with the  randomOne\n")
         return randomInteger
     }
+    //we create a function to exchange fighter's arm with the random's one
     func exchange()->Int{
         var damage : Int = 10
         damage = self.armStrenght()
